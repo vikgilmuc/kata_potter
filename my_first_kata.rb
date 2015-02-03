cd2170552176b35a92b7c37f6e7ac45b30b878b9
paul <paul@Victors-Macbook-2.local>
2015-02-03 10:54:53 +0100

    works but problem with different kind of lots

--- my_first_kata.rb
+++ my_first_kata.rb
@@ -1,10 +1,73 @@
 #my_first_kata
 
 	BOOK_PRICE = 8
-	DISCOUNT =[0,1, 0.95, 0.90, 0.80, 0.75]
+	DISCOUNT =[ 1, 1, 0.95, 0.90, 0.80, 0.75 ]
 
-def basket(*num)
-	num[1]=num[0] if num[1]==nil
+def basket(shop_items)
+
+puts shop_items.length
+#puts shop_items.uniq
+
+price = 0
+
+shop_items_by_index=[]
+
+(0..4).each { |i| shop_items_by_index[i]=shop_items.count(i+1)}
+
+shop_items_by_index.sort!
+
+
+
+
+
+
+
+
+
+
+(1..5).each {|v| 
+	puts "array #{v}    #{shop_items_by_index }"
+
+	if shop_items_by_index[v-1]!=0
+		lot_quantity=shop_items_by_index[v-1]
+		price+=calculate_discounted_price(6-v)*lot_quantity
+		puts price
+
+		shop_items_by_index.each_with_index{|x,i| 
+			puts i
+			puts x
+			shop_items_by_index[i]=shop_items_by_index[i]-lot_quantity if shop_items_by_index[i]>0
+			puts shop_items_by_index[i]}
+	end
+ }
+
+ price
+
+
+
+
+
+ 
+#while shop_items.uniq > 1
+#	shop_items
+#end
+#calculate_discounted_price(shop_items.uniq.length)
+
+
+
+end
+
+
+
+
+def calculate_regular_price(amount_of_books)
+
+BOOK_PRICE  * amount_of_books
+
+end
+
+
+def calculate_discounted_price(amount_of_books)
 	
-	BOOK_PRICE * ( DISCOUNT[num[1]] * (num[1])+  (num[0]-num[1])) 
+	BOOK_PRICE * DISCOUNT[amount_of_books] * amount_of_books
 end
--- my_first_kata_test.rb
+++ my_first_kata_test.rb
@@ -5,30 +5,39 @@ class TestPotter < Test::Unit::TestCase
 
 
   def test_single_book_cost_8_eur
-    assert_equal 8, basket(1)
+    assert_equal 8, basket([1])
   end
 
   def test_two_books_5_per_cent_discount
 
-  	assert_equal 15.2, basket(2)
+  	assert_equal 15.2, basket([1,2])
   end
 
   def test_three_books_10_per_cent_discount
 
-  	assert_equal 21.6, basket(3)
+  	assert_equal 21.6, basket([2,3,1])
   end
 
   def test_four_books_20_per_cent_discount
-  	assert_equal 25.6, basket(4)
+  	assert_equal 25.6, basket([5,1,3,4])
   end
   
   def test_five_books_25_per_cent_discount
-  	assert_equal 30, basket(5)
+  	assert_equal 30, basket([1,2,3,4,5])
   end
 
   def test_four_books_wich_3_diff_get_10_on_them
-  	assert_equal 29.6, basket(4,3)
+  	assert_equal 29.6, basket([1,2,3,3])
   end
+
+  def test_2_of_first_2_of_second_2_of_third_1_of_fourth_1_of_fifth
+  	assert_equal 51.20, basket([1,1,2,2,3,3,4,5])
+  end
+
+	def test_special
+  	assert_equal 51.20, basket([1,1,2,2,2,3,3,3,3,3,4,4,4,4,4,4,5,5,5,5,5,5,5])
+  end
+
 
 end
 
