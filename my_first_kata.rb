66b555645c5041b1018c316a1e31c6fbf1150119
Victor Gil <victor@vicgtorgil.name>
2015-02-09 10:40:24 +0100

    2 diff 2 equals refactored

--- my_first_kata.rb
+++ my_first_kata.rb
@@ -3,7 +3,6 @@ class Basket
 
   def initialize(books)
     @books = books
-    fill_books_when_empty
   end
 
   def calculate
@@ -16,20 +15,12 @@ class Basket
 
   private
 
-  def fill_books_when_empty
-    @books = [0]   if @books.empty?
-  end
 
   def calculate_non_discounted_items
-    if @books[0]!=0
     @books.length*PRICE
-    else
-    0
-    end
   end
 
   def calculate_discounted_items
-    puts "in_discounted "
     @books.uniq.length*PRICE*0.95
   end
 end
