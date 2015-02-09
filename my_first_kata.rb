547aa569ca893ffec199737f6b76af57d8467fa1
Victor Gil <victor@vicgtorgil.name>
2015-02-09 09:39:16 +0100

    before 2_dif_book_test

--- my_first_kata.rb
+++ my_first_kata.rb
@@ -1,108 +1,22 @@
-#my_first_kata
+class Basket
+  PRICE = 8
 
-	BOOK_PRICE = 8
-	DISCOUNT =[ 1, 1, 0.95, 0.90, 0.80, 0.75 ]
+  def initialize(books)
+    @books = books
+    fill_books_when_empty
+  end
 
-def basket(shop_items)
+  def calculate
+    first_item_in_basket * PRICE
+  end
 
+  private
 
-price = 0   # 
-price_array=[]   
-shop_items_by_index_original=[]
-mega_lots_array=[0,0,0,0]   #array of different combinations of discounts, beginning from lots of 5 to lots of 2 
+  def fill_books_when_empty
+    @books = [0]   if @books.empty?
+  end
 
-(0..4).each { |i| shop_items_by_index_original[i]=shop_items.count(i+1)}  # transform array of items to array of quantities
-
-
-shop_items_by_index_original.sort!.reverse!   # example [5,3,3,3,2] 5 of book x, 3 of book y, 
-
-
-(2..5).reverse_each {|v|       #   calculate lots beginning from 5 to 2      
-	
-	shop_items_by_index= shop_items_by_index_original.dup 
-	
-	lots_array=[0,0,0,0]       # array of 5,4,3,2  lots 
-	
-	if shop_items_by_index[v-1]!=0 
-
-
-		(2..v).reverse_each{|x| 
-
-						
-					while (shop_items_by_index[x-1]>0) 
-						
-						lots_array[5-x]+=1
-						take_out_a_lot(shop_items_by_index,x)
-						shop_items_by_index.sort!.reverse!
-													
-				    end  #while		 
-			    
-			}   # x reverse
-
-		
-			mega_lots_array[5-v]=lots_array
-
-		
-			# exceptional fall :  lots of 5 and 3 without lots of  4 = 2 x lots of 4
-		if (mega_lots_array[0][0]!=0)&&(mega_lots_array[0][2]!=0)&&(mega_lots_array[0][1]==0)
-
-			excep4lot=[mega_lots_array[0][0],mega_lots_array[0][2]].min
-			#puts excep4lot
-			mega_lots_array[0][1]+=(excep4lot)*2
-			mega_lots_array[0][0]-=excep4lot
-			mega_lots_array[0][2]-=excep4lot
-
-
-		end
-
-		
-		price_array[v-2]=(lots_array.each.with_index.inject(0){|sum,(val,index)| sum+calculate_discounted_price(5-index)*val} )+calculate_regular_price(shop_items_by_index[0])
-		
-		puts price_array[v-2]
-	
-
-
-	end   # if V-1
-
-	
-
-}  # v reverse
-
-	price_array.push((shop_items.length)*8)   # last element of price array without discounts						
-	price_array.min
-
-end
-
-
-def take_out_a_lot (shop_items_by_index_fun, x)
-
-
-			lot_counter=0
-
-				shop_items_by_index_fun.map!{|value|
-					if lot_counter<x
-					
-						lot_counter+=1
-						value-1
-					
-					elsif lot_counter==x
-							value			
-					end
-
-				}
-
-		
-end
-
-
-def calculate_regular_price(amount_of_books)
-
-	BOOK_PRICE  * amount_of_books
-
-end
-
-
-def calculate_discounted_price(amount_of_books)
-	
-	BOOK_PRICE * DISCOUNT[amount_of_books] * amount_of_books
+  def first_item_in_basket
+    @books.first
+  end
 end
--- my_first_kata_test.rb
+++ my_first_kata_test.rb
@@ -2,56 +2,27 @@ require "test/unit"
 require_relative "./my_first_kata"
 
 class TestPotter < Test::Unit::TestCase
-
-def test_no_book_cost_0_eur
-
-	
-    assert_equal 0, basket([])
+  def test_no_books_equal_0
+    assert_equal 0, calculate([]) 
   end
 
-
-
-def test_single_book_cost_8_eur
-
-    assert_equal 8, basket([1])
+  def test_1_book_equal_8
+    assert_equal 8, calculate([1])
   end
 
-  def test_two_books_5_per_cent_discount
-
-  	assert_equal 15.2, basket([1,2])
+  def test_2_books_equal_16
+    assert_equal 16, calculate([2])
   end
 
-  def test_three_books_10_per_cent_discount
-
-  	assert_equal 21.6, basket([2,3,1])
+  def test_3_books_equal_24
+    assert_equal 24, calculate([3])
   end
 
-  def test_four_books_20_per_cent_discount
-  	assert_equal 25.6, basket([5,1,3,4])
-  end
-  
-   def test_five_books_25_per_cent_discount
-  	assert_equal 30, basket([1,2,3,4,5])
-   end
-
-  def test_four_books_wich_3_diff_get_10_on_them
-   	assert_equal 29.6, basket([1,2,3,3])
-  end
-
- def test_2_of_first_2_of_second_2_of_third_1_of_fourth_1_of_fifth
-  	assert_equal 51.20, basket([1,1,2,2,3,3,4,5])
-  end
-
-
-	def test_special
-  	assert_equal 141.20, basket([1,1,1,1,1,2,2,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5])
-  end
-
-
-	def test_special_2
-  	assert_equal 151.20, basket([1,1,2,2,2,3,3,3,3,3,4,4,4,4,4,4,5,5,5,5,5,5,5])
-  end
-
-
+  def test_2_different_books_equal_16
+    assert_equal 16, calculate([1, 1])
+  end    
 end
 
+def calculate(basket)
+  Basket.new(basket).calculate
+end
