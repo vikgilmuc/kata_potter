0df49c064f4750380d2e1f01e97b76c88a1da23e
Victor Gil <victor@vicgtorgil.name>
2015-02-10 22:36:16 +0100

    all test ok

--- my_first_kata.rb
+++ my_first_kata.rb
@@ -3,22 +3,46 @@ class Basket
   DISCOUNTS =[0,0,0.05,0.10, 0.20, 0.25]
 
   def initialize(books)
+
     @books = books
+    @books_origin=@books.dup
     @accumulator= 0 
+    @packs=4
+    @flag=0
   end
 
   def calculate
-  
-      @accumulator += calculate_discounted_items 
-      @books.uniq.each{|x|@books.delete_at(@books.index(x))}
-      if @books.length > @books.uniq.length
-        calculate 
-      else
-        return @accumulator += calculate_non_discounted_items
+
+
+    if @books.length >= @books.uniq.length && @books.uniq.length > 1
+     
+     
+      uniq_1=@books.uniq.take(@packs)
+      @accumulator += calculate_discounted_items (uniq_1.length )
+      uniq_1.each{|x|  @books.delete_at(@books.index(x)) }
+      
+        calculate
+
+    else 
+
+      puts @accumulator
+      puts "books#{@books}"
+
+      if ((@accumulator == 25.6) &&  (@books==[1])) && @flag==0
+        @packs=5
+        @accumulator=0
+        @flag=1
+        @books=@books_origin
+        calculate
+        
       end
 
-    
+      return @accumulator += calculate_non_discounted_items
+    end
   end
+
+
+
 
   private
 
@@ -27,8 +51,8 @@ class Basket
     @books.length*PRICE
   end
 
-  def calculate_discounted_items
-    @books.uniq.length*PRICE*(1-DISCOUNTS[@books.uniq.length])
+  def calculate_discounted_items(pack_length)
+    pack_length*PRICE*(1-DISCOUNTS[pack_length])
   end
 
 
--- my_first_kata_test.rb
+++ my_first_kata_test.rb
@@ -23,7 +23,7 @@ class TestPotter < Test::Unit::TestCase
   def test_3_4_5_different_books_10_20_25_per_cent_discount
     assert_equal 21.6, calculate([1, 2, 3])
     assert_equal 25.6, calculate([1, 2, 3, 4])
-    assert_equal 30, calculate([1, 2, 3, 4, 5])
+    assert_equal 30, calculate([5, 4, 3, 2, 1])
   end   
 
 
@@ -38,6 +38,11 @@ class TestPotter < Test::Unit::TestCase
   def test_3_different_books_plus_2diff_plus_8_changed_order
     assert_equal 44.8 , calculate([3, 2, 2, 1, 1 , 1])
   end 
+
+  def test_5_3_diff_books_vs_4_4_books
+    assert_equal 51.2 , calculate([1,1,2,2,3,3,4,5])
+  end
+
 
 
 end
